13743f62ed30924988f68e8e21206398ecfb5838
Andreas Eriksson <andreas.eriksson@glesys.se>
2020-10-02 16:42:40 +0200

    Update examples to terraform 0.12 syntax
    * vmware
    * networks
    * loadbalancer

--- examples/loadbalancer/example.tf
+++ examples/loadbalancer/example.tf
@@ -1,31 +1,31 @@
 resource "glesys_loadbalancer" "mylb" {
-  count = 1
+  count      = 1
   datacenter = "Falkenberg"
-  name = "mylb-1"
+  name       = "mylb-1"
 }
 
 resource "glesys_loadbalancer_backend" "mybackend" {
-  loadbalancerid = "${glesys_loadbalancer.mylb.id}"
-  name = "my-web-backend"
-  mode = "tcp"
+  loadbalancerid = glesys_loadbalancer.mylb[0].id
+  name           = "my-web-backend"
+  mode           = "tcp"
   connecttimeout = 20000
 }
 
 resource "glesys_loadbalancer_frontend" "myfrontend" {
-  loadbalancerid = "${glesys_loadbalancer.mylb.id}"
-  name = "web-fe91"
-  backend = "${glesys_loadbalancer_backend.mybackend.id}"
-  port = 80
+  loadbalancerid = glesys_loadbalancer.mylb[0].id
+  name           = "web-fe91"
+  backend        = glesys_loadbalancer_backend.mybackend.id
+  port           = 80
 }
 
 resource "glesys_loadbalancer_target" "web111" {
-  loadbalancerid = "${glesys_loadbalancer.mylb.id}"
-  backend = "${glesys_loadbalancer_backend.mybackend.id}"
+  loadbalancerid = glesys_loadbalancer.mylb[0].id
+  backend        = glesys_loadbalancer_backend.mybackend.id
 
-  name = "web111"
-  port = 8898
+  name     = "web111"
+  port     = 8898
   targetip = "172.16.0.10"
-  weight = 15
+  weight   = 15
 
   enabled = false
 }
