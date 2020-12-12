be4dddb771538a9eafdac895ef3bdbfe75040f02
Mikael Svantesson <mikael@distopic.net>
2020-12-12 17:12:13 +0100

    Replace "fbg" with "sto" for dc-sto1 resource

--- examples/objectstorage/example.tf
+++ examples/objectstorage/example.tf
@@ -1,11 +1,11 @@
-resource "glesys_objectstorage_instance" "objectstore_example_fbg" {
+resource "glesys_objectstorage_instance" "objectstore_example_sto" {
   count = 1
   datacenter = "dc-sto1"
-  description = "tf-test-fbg-${count.index}-2"
+  description = "tf-test-sto-${count.index}-2"
 }
 
-resource "glesys_objectstorage_credential" "objectstore_example_fbg_cred" {
+resource "glesys_objectstorage_credential" "objectstore_example_sto_cred" {
   count = 1
-  instanceid = glesys_objectstorage_instance.objectstore_example_fbg[0].id
-  description = "tf-test-fbg-cred-${count.index}"
+  instanceid = glesys_objectstorage_instance.objectstore_example_sto[0].id
+  description = "tf-test-sto-cred-${count.index}"
 }
