f57eb038cc769304acee5156a1e0eb80a4826c6b
Andreas Eriksson <andreas.eriksson@glesys.se>
2021-02-11 14:28:30 +0100

    add comment to func

--- glesys/supress.go
+++ glesys/supress.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
+// IgnoreCase check if the strings match when both are in lowercase
 func IgnoreCase(_, old, new string, _ *schema.ResourceData) bool {
 	return strings.ToLower(old) == strings.ToLower(new)
 }
