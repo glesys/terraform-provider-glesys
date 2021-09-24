6cbb0f034844b8f28cfa2a7b057d8d41f4cf2edf
lecs <33053602+larsve@users.noreply.github.com>
2021-09-24 12:54:04 +0200

    Fixes KVM template mismatch

---
+++ glesys/resource_glesys_server_test.go
@@ -0,0 +1,42 @@
+package glesys
+
+import (
+	"testing"
+
+	"github.com/glesys/glesys-go/v2"
+)
+
+func Test_getTemplate(t *testing.T) {
+	srv := &glesys.ServerDetails{}
+	for _, tt := range []struct {
+		name         string
+		tfTemplate   string
+		readTemplate string
+		readTags     []string
+		want         string
+	}{
+		{
+			name:         "KVM_instance",
+			tfTemplate:   "ubuntu-20-04",
+			readTemplate: "Ubuntu 20.04 LTS (Focal Fossa)",
+			readTags:     []string{"ubuntu", "ubuntu-lts", "ubuntu-20-04"},
+			want:         "ubuntu-20-04",
+		},
+		{
+			name:         "VMware_instance",
+			tfTemplate:   "Ubuntu 20.04 LTS 64-bit",
+			readTemplate: "Ubuntu 20.04 LTS 64-bit",
+			readTags:     []string{},
+			want:         "Ubuntu 20.04 LTS 64-bit",
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			srv.Template = tt.readTemplate
+			srv.InitialTemplate.Name = tt.readTemplate
+			srv.InitialTemplate.CurrentTags = tt.readTags
+			if got := getTemplate(tt.tfTemplate, srv); got != tt.want {
+				t.Errorf("got: %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
