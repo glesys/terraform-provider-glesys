18e8a486b7b44ff0d0036ece425d335facedec23
Andreas Eriksson <andreas.eriksson@glesys.se>
2022-09-12 15:24:28 +0200

    Add documentation for glesys_dnsdomain
    * Add description to schema attributes.
    * Regenerate docs

---
+++ examples/resources/glesys_dnsdomain/resource.tf
@@ -0,0 +1,10 @@
+resource "glesys_dnsdomain" "mydomain" {
+  name = "example.com"
+}
+
+resource "glesys_dnsdomain_record" "www" {
+  domain = glesys_dnsdomain.mydomain.id
+  data   = "127.0.0.1"
+  host   = "www"
+  type   = "A"
+}
