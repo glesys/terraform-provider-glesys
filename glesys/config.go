e436b39ed9955ff72cf084dad5fd9ef5699d17a3
Andreas Eriksson <andreas.eriksson@glesys.se>
2022-11-01 11:50:35 +0100

    Release v0.8.0

--- glesys/config.go
+++ glesys/config.go
@@ -14,7 +14,7 @@ type Config struct {
 
 // Client - Setup new glesys client
 func (c *Config) Client() (*glesys.Client, error) {
-	client := glesys.NewClient(c.UserID, c.Token, "tf-glesys/0.7.1")
+	client := glesys.NewClient(c.UserID, c.Token, "tf-glesys/0.8.0")
 
 	err := client.SetBaseURL(c.APIEndpoint)
 	if err != nil {
