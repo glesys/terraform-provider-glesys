b7dce5969a4434a6886ee940c7a097edeedc0ecb
Andreas Eriksson <andreas.eriksson@glesys.se>
2024-10-02 09:30:33 +0200

    Add glesys_server_disk acctest

---
+++ glesys/resource_glesys_server_disk_test.go
@@ -0,0 +1,40 @@
+package glesys
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccServerDiskVMware_basic(t *testing.T) {
+	rName := acctest.RandomWithPrefix("tf-vmw-disk")
+	sName := acctest.RandomWithPrefix("tf-srv-vmw")
+
+	name := "glesys_server_disk.test"
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testGlesysProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGlesysServerBase_VMware(sName) + testAccGlesysServerDisk_VMware(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "name", rName),
+					resource.TestCheckResourceAttr(name, "size", "20"),
+					resource.TestCheckResourceAttr(name, "type", "silver"),
+				),
+			},
+		},
+	})
+}
+
+func testAccGlesysServerDisk_VMware(name string) string {
+	return fmt.Sprintf(`
+		resource "glesys_server_disk" "test" {
+			serverid = glesys_server.test.id
+			name     = "%s"
+			size     = 20
+			type     = "silver"
+		} `, name)
+}
