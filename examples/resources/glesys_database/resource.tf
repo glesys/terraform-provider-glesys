0db619553d1f1e74148a501f4572d501b3cea969
Andreas Eriksson <andreas.eriksson@glesys.se>
2025-10-03 11:17:25 +0200

    Add documentation for glesys_database

---
+++ examples/resources/glesys_database/resource.tf
@@ -0,0 +1,8 @@
+resource "glesys_database" "mydb" {
+  datacenterkey = "dc-fbg1"
+  name          = "tf-test1"
+  engine        = "mysql"
+  engineversion = "8.0"
+  plankey       = "plan-1core-4gib-25gib"
+  allowlist     = ["127.0.0.1", "127.0.0.2"]
+}
